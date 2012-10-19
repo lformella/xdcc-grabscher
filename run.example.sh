0b6a59db54840e323e891da79a9913536c7f68d0
Lars Formella <ich@larsformella.de>
2012-10-20 00:14:04 +0200

    better start / stop scripts

--- run.example.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mono XG.Server.Cmd.exe > log.txt & pid=$!
-echo $pid > pid
-
---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+DIR="$( dirname "$SOURCE" )"
+while [ -h "$SOURCE" ]
+do 
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+  DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd )"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+cd $DIR
+mono Server.Cmd.exe > log.txt & pid=$!
+echo $pid > pid
+
